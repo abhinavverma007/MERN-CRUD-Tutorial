6b9fab2dd193a0f931675750b082dfd5bef60f54
Abhinav Verma <verma.kirti111@gmail.com>
2021-03-17 07:13:42 +0530

    GET AND POST

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+// import to wrap in curly brace else error
+import {useState} from 'react';
+import {useEffect} from 'react';
+// library for sending HTTP requests
+import Axios from 'axios';
 
 function App() {
+  const [foodName,setFoodName]=useState("");
+  const [days,setDays]=useState(0);
+  const [foodList,setFoodList]=useState([]);
+
+   // will run everytime page refreshes
+  useEffect( ()=>{
+    Axios.get('http://localhost:3001/read').then( (response)=> {
+      setFoodList(response.data);
+    });
+  }, [] );
+  const addToList = () => {
+    Axios.post("http://localhost:3001/insert" , { nameOfFood: foodName, numberOfDays: days});
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1> CRUD app with MERN </h1>
+      <label> Food Name: </label>
+      <input type="text" onChange={ (event)=> {
+        setFoodName(event.target.value)
+      }}/>
+      <label> Days Since You ate It : </label>
+      <input type="number" onChange={ (event)=> {
+        setDays(event.target.value)
+      }} />
+      <button onClick={addToList}> Add to List</button>
+      <h1> Food List </h1>
+      
+      {foodList.map( ( val , key )=> {
+        return <div key={key}> <h1>{ val.foodName} </h1> <h1> { val.daysSinceIAte}</h1></div>
+      })}
+      </div>
   );
 }
 
--- server/index.js
+++ server/index.js
@@ -1,18 +1,22 @@
 const express=require('express');
 const app=express();
 const mongoose=require('mongoose');
-
+// for self made calls to backend api
+const cors=require('cors');
 // we need our schema in here
 const foodModel=require('./models/Food');
 // receive information from front-end in json format
 app.use(express.json());
+app.use(cors());
 
 mongoose.connect("mongodb+srv://newUser:zabGvYzQuGX4suNu@crud.qef6s.mongodb.net/food?retryWrites=true&w=majority", {
     useNewUrlParser: true
 });
 
-app.get('/', async (req,res)=>{
-    const food=new foodModel({foodName: 'Apple', daysSinceIAte: 3});
+app.post('/insert', async (req,res)=>{
+    const namefood=req.body.nameOfFood;
+    const countDays=req.body.numberOfDays;
+    const food=new foodModel({foodName: namefood, daysSinceIAte: countDays});
     try{
         // telling database to save
         await food.save();
@@ -20,7 +24,16 @@ app.get('/', async (req,res)=>{
     }catch(err){
         console.log(err);
     }
-})
+});
+app.get("/read",async (req,res)=>{
+    // find {} empty because we want all data
+    foodModel.find({}, (err,result)=> {
+        if(err){
+            res.send(err);
+        }
+        res.send(result);
+    });
+});
 app.listen(3001,()=>{
     console.log("Server running on port 3001");
 });
